bc0905d7474af6d80b5db9b9535a71b635688fbc
Igor Nesterenya <nesterione@gmail.com>
2015-07-24 21:51:44 +0300

    Parse 30 ads

--- coreservices/src/main/java/com/nesterenya/services/HatubyParserService.java
+++ coreservices/src/main/java/com/nesterenya/services/HatubyParserService.java
@@ -19,53 +19,58 @@ public class HatubyParserService implements Parser {
 	public List<Ad> parse() {
 
 		List<Ad> ads = new ArrayList<>();
-		
+
 		try {
-            String url = "http://www.hatu.by/";
-            Document document = Jsoup.connect(url).get();
-            Element element = document.getElementById("list");
-            Elements elements = element.getElementsByClass("box-container");
+			for (int i = 0; i < 3; i++) {
 
-            
+				String url = "http://www.hatu.by/ad/search/minsk/" + i + "/list.do";
+				Document document = Jsoup.connect(url).get();
+				Element element = document.getElementById("list");
+				Elements elements = element.getElementsByClass("box-container");
 
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+						"dd.MM.yyyy");
 
-            for (Element e : elements) {
+				for (Element e : elements) {
 
-                Ad ad = new Ad();
-                Element a = e.getElementsByTag("h2").first().getElementsByTag("a").first();
-                ad.setAddress(a.text());
+					Ad ad = new Ad();
+					Element a = e.getElementsByTag("h2").first()
+							.getElementsByTag("a").first();
+					ad.setAddress(a.text());
 
-                String price = e.getElementsByClass("price").first().text();
-                ad.setCost(price);
+					String price = e.getElementsByClass("price").first().text();
+					ad.setCost(price);
 
-                Element note = e.getElementsByClass("note").first();
-                Elements strongs = note.getElementsByTag("strong");
+					Element note = e.getElementsByClass("note").first();
+					Elements strongs = note.getElementsByTag("strong");
 
-                String stringDate = strongs.get(0).text();
-                Date date = simpleDateFormat.parse(stringDate);
-                ad.setDate(date);
+					String stringDate = strongs.get(0).text();
+					Date date = simpleDateFormat.parse(stringDate);
+					ad.setDate(date);
 
-                String stringCnt = strongs.get(1).text();
-                int roomCnt = Integer.parseInt(stringCnt);
-                ad.setRoomCount(roomCnt);
+					String stringCnt = strongs.get(1).text();
+					int roomCnt = Integer.parseInt(stringCnt);
+					ad.setRoomCount(roomCnt);
 
-                String discr = e.getElementsByAttributeValue("itemprop","description").text();
-                ad.setDescription(discr);
+					String discr = e.getElementsByAttributeValue("itemprop",
+							"description").text();
+					ad.setDescription(discr);
 
-                String contact = e.getElementsByClass("psevdo").get(1).text();
-                ad.setContacts(contact);
-                ads.add(ad);
+					String contact = e.getElementsByClass("psevdo").get(1)
+							.text();
+					ad.setContacts(contact);
+					ads.add(ad);
 
-                ad.setSource("http://www.hatu.by/");
-            }
+					ad.setSource("http://www.hatu.by/");
+				}
+			}
 
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-		
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
 		return ads;
 	}
 
